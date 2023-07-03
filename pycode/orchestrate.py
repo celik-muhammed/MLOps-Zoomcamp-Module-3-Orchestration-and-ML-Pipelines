6570c92d1d7bdb8895a1afb1df15f05a268654b5
celik-muhammed <developerghost01@gmail.com>
2023-07-03 16:04:34 +0300

    Module-3-Orchestration-and-ML-Pipelines

--- pycode/orchestrate.py
+++ pycode/orchestrate.py
@@ -225,32 +225,6 @@ def download_data(raw_data_path: str, years: list, months: list, colors: list):
                 
 
 @flow(name="Main Flow")
-@click.command()
-@click.option(
-    "--raw_data_path",
-    default="./data",
-    help="Location where the raw NYC taxi trip data was saved"
-)
-@click.option(
-    "--dest_path",
-    default="./models",
-    help="Location where the resulting model files will be saved"
-)
-@click.option(
-    "--years",
-    default="2023",
-    help="Years where the raw NYC taxi trip data was saved (space-separated)"
-)
-@click.option(
-    "--months",
-    default="1 2 3 4",
-    help="Months where the raw NYC taxi trip data was saved (space-separated)"
-)
-@click.option(
-    "--colors",
-    default="green yellow",
-    help="Colors where the raw NYC taxi trip data was saved"
-)
 def main_flow(raw_data_path: str, dest_path: str, years: str, months: str, colors: str) -> None:
     """The main training pipeline"""
     # MLflow settings
@@ -289,4 +263,12 @@ def main_flow(raw_data_path: str, dest_path: str, years: str, months: str, color
     # example_email_send_message_flow(['@gmail.com'])
 
 if __name__ == "__main__":
-    main_flow()
+    parser = argparse.ArgumentParser(description="Main Flow")
+    parser.add_argument("--raw_data_path", default="./data", help="Location where the raw NYC taxi trip data was saved")
+    parser.add_argument("--dest_path", default="./models", help="Location where the resulting model files will be saved")
+    parser.add_argument("--years", default="2023", help="Years where the raw NYC taxi trip data was saved (space-separated)")
+    parser.add_argument("--months", default="1 2 3 4", help="Months where the raw NYC taxi trip data was saved (space-separated)")
+    parser.add_argument("--colors", default="green yellow", help="Colors where the raw NYC taxi trip data was saved")
+
+    args = parser.parse_args()
+    main_flow(args.raw_data_path, args.dest_path, args.years, args.months, args.colors)
